14e44394896370025a0d9f994483fa460eba6f47
malekcoders <alomayri970@outlook.com>
2024-03-04 12:17:04 +0300

    edit security filter chain

--- src/main/java/com/malek/app/rest/security/SecurityConfiguration.java
+++ src/main/java/com/malek/app/rest/security/SecurityConfiguration.java
@@ -1,6 +1,9 @@
 package com.malek.app.rest.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.transaction.TransactionDefinition.withDefaults;
 
@@ -9,10 +12,11 @@ public class SecurityConfiguration {
 
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
+               .csrf().disable()
+               .authorizeRequests()
+               .anyRequest().authenticated()
+               .and()
+               .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 
